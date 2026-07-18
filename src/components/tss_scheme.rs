3b9dbbe9a1ebc079342e1dbbddf86c44960eea92
Viktor Starodubtsev <viktorst2005@gmail.com>
2026-07-18 15:48:21 +0400

    feat: Scalable TSS Scheme.

--- src/components/tss_scheme.rs
+++ src/components/tss_scheme.rs
@@ -1,3 +1,5 @@
+use egui::Scene;
+
 use crate::{app::SimulationUpdate, components::settings::SettingsComponent};
 
 pub fn render(
@@ -7,35 +9,61 @@ pub fn render(
     settings: &SettingsComponent,
 ) {
     ui.heading("TSS Scheme");
-    let last_state = history.last().map(|u| u.state);
+    let size = ui.available_size();
+    let scene_rect_id = ui.id().with("tss_scene_rect");
 
-    let size = egui::vec2(ui.available_width(), ui.available_height());
-    let (rect, _) = ui.allocate_exact_size(size, egui::Sense::hover());
-    let painter = ui.painter_at(rect);
-    let center = rect.center();
+    let mut scene_rect = ui.data_mut(|d| {
+        *d.get_temp_mut_or_insert_with(scene_rect_id, || {
+            egui::Rect::from_center_size(egui::Pos2::ZERO, size)
+        })
+    });
 
-    if let Some(state) = last_state {
-        let r = state.l / f64::sqrt(2.0);
-        let scale = 0.4 * f64::from(rect.width()) / settings.config.l_k;
-        let angles = [0.0, 1.0, 2.0, 3.0].map(|i| state.theta + i * std::f64::consts::FRAC_PI_2);
-        let points: Vec<egui::Pos2> = angles
-            .iter()
-            .map(|&a| {
-                center + egui::vec2((r * a.cos() * scale) as f32, (r * a.sin() * scale) as f32)
-            })
-            .collect();
-        let tension = history.last().map(|u| u.tension).unwrap_or(0.0);
-        let line_color = if tension == 0.0 {
-            egui::Color32::from_rgb(100, 149, 237)
-        } else if tension > 50.0 {
-            egui::Color32::from_rgb(220, 20, 60)
-        } else {
-            egui::Color32::from_rgb(50, 205, 50)
-        };
+    /*
+        Add `.scroll_zooms(true)` if https://github.com/emilk/egui/pull/7891 gets approved someday.
 
-        for i in 0..4 {
-            painter.line_segment([points[i], points[(i + 1) % 4]], (2.0, line_color));
-            painter.circle_filled(points[i], 8.0, egui::Color32::GRAY);
-        }
-    }
+        This method brings in better UX: user can scale a `Scene` without holding `Ctrl`.
+        So far, `Scene`-scaling can only be done when holding `Ctrl`.
+    */
+    Scene::new()
+        .zoom_range(0.1..=2.0)
+        .sense(egui::Sense::hover())
+        .max_inner_size(size)
+        .show(ui, &mut scene_rect, |ui| {
+            let painter = ui.painter();
+            let last_state = history.last().map(|u| u.state);
+
+            if let Some(state) = last_state {
+                let r = state.l / f64::sqrt(2.0);
+
+                let base_virtual_width = size.x as f64;
+                let scale = 0.4 * base_virtual_width / settings.config.l_k;
+
+                let angles =
+                    [0.0, 1.0, 2.0, 3.0].map(|i| state.theta + i * std::f64::consts::FRAC_PI_2);
+                let points: Vec<egui::Pos2> = angles
+                    .iter()
+                    .map(|&a| {
+                        egui::Pos2::new((r * a.cos() * scale) as f32, (r * a.sin() * scale) as f32)
+                    })
+                    .collect();
+
+                let tension = history.last().map(|u| u.tension).unwrap_or(0.0);
+                let line_color = if tension == 0.0 {
+                    egui::Color32::from_rgb(100, 149, 237)
+                } else if tension > 50.0 {
+                    egui::Color32::from_rgb(220, 20, 60)
+                } else {
+                    egui::Color32::from_rgb(50, 205, 50)
+                };
+
+                for i in 0..4 {
+                    painter.line_segment([points[i], points[(i + 1) % 4]], (2.0, line_color));
+                    painter.circle_filled(points[i], 8.0, egui::Color32::GRAY);
+                }
+            }
+        });
+
+    let centered_scene_rect = egui::Rect::from_center_size(egui::Pos2::ZERO, scene_rect.size());
+
+    ui.data_mut(|d| d.insert_temp(scene_rect_id, centered_scene_rect));
 }
